dd292ec5c4462aacab41a57f27cc9c99193e5377
Assesment NBL <kamble_rahulg@yahoo.co.in>
2025-06-01 12:53:58 +0530

    testing js

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -9,6 +9,7 @@ export const CartProvider = ({ children }) => {
     setCartItems((prev) => {
       const exists = prev.find((item) => item.id === product.id);
       if (exists) {
+        
         return prev.map((item) =>
           item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
         );
